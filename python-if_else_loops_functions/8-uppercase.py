d43f54305fbb56e8289f98c5ac6cfbed6ff4c6b6
sinannrzd8 <sinannrzd.2008@gmail.com>
2026-04-08 08:00:25 +0000

    Task8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        if 97 <= ord(c) <= 122:
+            print("{}".format(chr(ord(c) - 32)), end="")
+        else:
+            print("{}".format(c), end="")

5c8c1b029e028d4dc006fb4e08614865c1ec50e8
sinannrzd8 <sinannrzd.2008@gmail.com>
2026-04-11 19:44:15 +0000

    end of module all tasks

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+
+    best_key = None
+    max_value = float('-inf')
+
+    for key, value in a_dictionary.items():
+        if value > max_value:
+            max_value = value
+            best_key = key
+
+    return best_key

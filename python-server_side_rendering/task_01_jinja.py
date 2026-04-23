6bb9c0e1e5a0bb65b83c6574b12dfbc72efde330
sinannrzd8 <sinannrzd.2008@gmail.com>
2026-04-23 07:14:31 +0000

    end

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -21,3 +21,9 @@ def contact():
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+~                                                                                                                                                                                                                                   
+~                                                                                                                                                                                                                                   
+~                                                                                                                                                                                                                                   
+~                                                                                                                                                                                                                                   
+~                                                                                                                                                                                                                                   
+~                                    

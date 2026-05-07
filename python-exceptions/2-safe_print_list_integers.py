2e19c9201df1ec0a47fbc455e9935fa2a7c9517a
sinannrzd8 <sinannrzd.2008@gmail.com>
2026-05-07 11:43:02 +0000

    Task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,12 +6,14 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             item = my_list[i]
+        except IndexError:
+            break
+
+        try:
             print("{:d}".format(item), end="")
             count += 1
         except (ValueError, TypeError):
             continue
-        except IndexError:
-            break
 
     print()
     return count

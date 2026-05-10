d7ff8d905541a84961bf593894488158ca8de22d
sinannrzd8 <sinannrzd.2008@gmail.com>
2026-05-10 23:16:25 +0400

    Task 5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Defines a square."""
+
+
 class Square:
+    """Represents a square."""
+
     def __init__(self, size=0):
         self.size = size
 
@@ -11,8 +16,10 @@ class Square:
     def size(self, value):
         if type(value) is not int:
             raise TypeError("size must be an integer")
+
         if value < 0:
             raise ValueError("size must be >= 0")
+
         self.__size = value
 
     def area(self):
@@ -20,8 +27,8 @@ class Square:
 
     def my_print(self):
         if self.__size == 0:
-            print("")
+            print()
             return
 
-        for i in range(self.__size):
+        for _ in range(self.__size):
             print("#" * self.__size)

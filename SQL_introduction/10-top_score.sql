86edac78393aeac8b473cf3e16a7cda41971f0c6
sinannrzd8 <sinannrzd.2008@gmail.com>
2026-05-13 16:53:05 +0400

    END SQL

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- lists all records ordered by score (descending)
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- creates table first_table in current database
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);

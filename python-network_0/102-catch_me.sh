265392fa9ab4488d6244402649119376ac403b71
sinannrzd8 <sinannrzd.2008@gmail.com>
2026-05-16 12:47:37 +0400

    end

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Makes a request that makes the server return "You find me!"
-curl -sL -X PUT -H "X-School-User-Id: 98" 0.0.0.0:5000/catch_me
+# Sends a request that makes the server respond "You find me!"
+curl -sL -X PUT -H "X-School-User-Id: 98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me

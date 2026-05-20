843b278dd469d50ec96724886f2b4659d948d8cc
sinannrzd8 <sinannrzd.2008@gmail.com>
2026-05-20 15:09:13 +0400

    task 2

---
+++ python-network_1/hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from urllib import request
+
+with request.urlopen('https://intranet.hbtn.io/status') as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))

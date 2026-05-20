ebac0bbdd91be9428c4146aa0ae7190e5d9a031e
sinannrzd8 <sinannrzd.2008@gmail.com>
2026-05-20 20:35:58 +0400

    end

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-word = "Holberton School"
-print(word[8] + word[7] + word[4] + word[1:3] + word[3] + word[6] + word[12] + word[0] + word[2:4])
+str = "object-oriented programming with Python"
+
+print(str)

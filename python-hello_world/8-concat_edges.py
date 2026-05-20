c9f8bc7e49a3a7f0ab66b1968bb9926aebcbf411
sinannrzd8 <sinannrzd.2008@gmail.com>
2026-05-20 20:33:48 +0400

    ,

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 word = "Holberton School"
-print(word[:3] + word[7:8] + word[1:3] + word[4:5] + word[6:7])
+print(word[8] + word[7] + word[4] + word[1:3] + word[3] + word[6] + word[12] + word[0] + word[2:4])
